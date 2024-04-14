42838ce35cf9049c2164914cfcedef7caf2d7538
AksBoi <akshatsurana787@gmail.com>
2024-04-15 02:08:03 +0530

    ps chng

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-const URL = "mongodb+srv://akshatsurana787:9LYN9wJvw75787lh@stundenthiringapp.3xjhx7t.mongodb.net/?retryWrites=true&w=majority&appName=StundentHiringApp";
+const URL = "mongodb+srv://akshatsurana787:jptMzdaDzJt2YvQ6@stundenthiringapp.3xjhx7t.mongodb.net/?retryWrites=true&w=majority&appName=StundentHiringApp";
 
 mongoose
 	.connect(URL, { useNewUrlParser: true })
